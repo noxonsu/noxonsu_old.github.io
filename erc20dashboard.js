78d3572c0bac1238418844abd5151ea3e489bb10
Noxon <myzonemailru@gmail.com>
2017-06-03 19:07:47 +0200

    Update erc20dashboard.js

--- erc20dashboard.js
+++ erc20dashboard.js
@@ -1,6 +1,6 @@
 
 	if (typeof erc20contract_address == "undefined") {
-		var erc20contract_address = "0x8E22892c308812F9bCdF67cf34a9e94227eb3ce9";
+		var erc20contract_address = "0x9671Bf5124Cb4Bd30027152FAFe8409eA46677E4";
 		var option_etherscan_api = 'https://ropsten.etherscan.io'; //change to https://api.etherscan.io for mainnet
 		var option_registration_enabled = true;
 		var option_registration_backend = '';///'subscribe.php'; //you can use remote address like https://yoursite.com/subscribe.php
